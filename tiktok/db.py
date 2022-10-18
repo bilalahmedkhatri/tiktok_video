8c06408ae4633a1a002d8dbc68bb3e063d96baec
bilalahmedkhatri <bilalahmedkhatri@outlook.com>
2022-10-19 04:04:18 +0500

    working on video id

--- tiktok/db.py
+++ tiktok/db.py
@@ -1,13 +1,15 @@
+from email.header import Header
+from xmlrpc.client import DateTime
 import psycopg2
-from datetime import datetime
+from datetime import date, datetime
 
-dd = ['trending', 'foryou', 'foryoupage', 'fyp', 'storytime', 'coming2america', 'treding', 'team07', 'viralvideo', 'moment', 'lovestory', 'view', 'foryou', 'foryoupage', 'fyp', 'tiktok', 'viral', 'funny', 'trend', 'trending', 'funny', 'lol', 'fyp', 'trending', 'foryoupage', 'sayitright', 'fyp', 'biginkenergy', 'petsoftiktok', 'petlife', 'viral', 'trending', '', 'bellletstalk', 'xgamesmode', 'xyzbca', 'asmr', 'satisfying', 'trending', 'viral', 'fyp', 'foryou', 'foryoupage', '4u', 'duet', 'fypシ', 'fypg', 'fu', 'arlostrong', 'fypシ', '4yp', 'trending', 'viral', 'k9softiktok', 'tiktokdogs', '', 'fyp', 'viral', 'gnb', 'share', 'trend', 
-'trending', 'verifyme', 'couple', 'foryou', 'fyp', 'viral', 'baby', 'mommydaughter', 'fypシ', 'trending', 'foryou', 'foryoupage', 'fyp', 'viral', 'trending', 'hospital', 'foryou', 'foryoupage', 'trending', 'fypシ', 'xyzbca', 'earthday', 'foryourpage', 'fup', '', '', '', '', '', 'whamos', 'fyp', 'trending', 'fyp', 'foryoupage', 'fypage', '2022', 'blessed', 'trending', 'zenitsu', 
-'demonslayer', 'wtf', 'awesome', 'cool', 'viral', 'wtf', 'happy', 'yes', 'blowup', 'fypageシ', 'pov', 'duet', 'yes', 'thankyou', 'loveyou', 'cosplay', 'workout', 'motivation', 'fyp', 'tourettes', 'foryou', 'tourettesawareness', 'lockdown', 'viral', 'funny', 'trending', 'painting', 'artistsoftiktok', 'trending', 'fyp', '', 'foryou', 'fyp', 'foryoupage', 'trending', 'viral', 'tennis', 'football', 'tictactoe', 'challenge', 'xyzbca', 'funny', 'sports', 'foryou', 'fyp', 'foryoupage', 'tiktok', 'comedy', 'trending', 'trend', 'funny', '', 'stitch', 'fyp', 'foryoupage', 'lenovojustbeyou', 'petlife', 'petsoftiktok', 'trending', 'fyp', 'funny', 'babyanddad', 'cutetoddler', 'viral', 'trending', 'bunnies', 'foryou', 'girldad', 'comedy', 'trend', '4u', 'trending', 
-'fyp', 'fypage', 'twilightsaga', 'usman', 'usman', 'usman', 'usman', 'usman', 'usman', 'usman', 'usman', 'bilal', 'taylorlautner', 'taydome', 'robertpattinson', 'viral', 'couplegoals', 'fyp', 'foryou', 'mellocair', 'wdym', 'whatdoyoumeme', 'trending', 'homecooked', 'cashappinbio', 'minecraft', 'foryou', 'foryoupage', 'minecraftbuild', 'mc', 'build', 'viral', 'trending', 'fernandoarmy', 'foryou', 'fy', 'fouryoupage', 'viral', 'trend', 'trending', 'paperplane', 'tutorial', '', '', 'dancer', 'choreography', 'trending', 'slumberparty', 'ashnikko', 'lgbt', 'brisbane', 'twerk', 'kmstudios', 'australia', 'dancetok', 'justiceforjohnnydepp', 'partnerwork', 'lesbian', '', 'trending', 'featureme', 'featurethis', 'viral', 'fyp', 'comedy', 'foryoupage', 'foryou', 'animal', 'lion', 'tiger', 'fyp', 'edit', 'pet', 'cat', 'fup', 'animals', 'nature', 'wild', 'wildlife', 'viral', 'zoo', 'trend', 'trending', 'safari', 'foru', 'awareness', 'learn', 'learnontiktok', 'edutok', 'education', '', 'ctcvoicebox', 'dayinmylife', 'oikosonetrip', 'rock', 'soul', 'fyp', 'cover', '365days', 'iseered', 'sing', 'singer', 'foryou', 'viral', 'trending', 'xyzbca', 'fyp', 'foryou', 'foryoupage', 'catsoftiktok', 'fantheory', 'tiktokfitness', 'trending', 'duet', '', 'satisfying', 'asmr', 'crushing', 'fyp', 'crushit', 'foryoupage', 'glass', 'trending', 'foryou', 'fyp', 'foryoupage', 'fyp', 'tiktok', 'viral', 'funny', 'trending', 'trend', 'fyp', 'fypシ', 'funny', 'funnyclip', 'meme', 'comedy', 'viral', 'trending', 'strange', 'lmao', 'hilarious', 'dailydoseofinternet', 'xyzabc']
+dd = ['trending', 'foryou', 'foryoupage', 'fyp', 'storytime', 'coming2america', 'treding', 'team07', 'viralvideo', 'moment', 'lovestory', 'view', 'foryou', 'foryoupage', 'fyp', 'tiktok', 'viral', 'funny', 'trend', 'trending', 'funny', 'lol', 'fyp', 'trending', 'foryoupage', 'sayitright', 'fyp', 'biginkenergy', 'petsoftiktok', 'petlife', 'viral', 'trending', '', 'bellletstalk', 'xgamesmode', 'xyzbca', 'asmr', 'satisfying', 'trending', 'viral', 'fyp', 'foryou', 'foryoupage', '4u', 'duet', 'fypシ', 'fypg', 'fu', 'arlostrong', 'fypシ', '4yp', 'trending', 'viral', 'k9softiktok', 'tiktokdogs', '', 'fyp', 'viral', 'gnb', 'share', 'trend',
+      'trending', 'verifyme', 'couple', 'foryou', 'fyp', 'viral', 'baby', 'mommydaughter', 'fypシ', 'trending', 'foryou', 'foryoupage', 'fyp', 'viral', 'trending', 'hospital', 'foryou', 'foryoupage', 'trending', 'fypシ', 'xyzbca', 'earthday', 'foryourpage', 'fup', '', '', '', '', '', 'whamos', 'fyp', 'trending', 'fyp', 'foryoupage', 'fypage', '2022', 'blessed', 'trending', 'zenitsu',
+      'demonslayer', 'wtf', 'awesome', 'cool', 'viral', 'wtf', 'happy', 'yes', 'blowup', 'fypageシ', 'pov', 'duet', 'yes', 'thankyou', 'loveyou', 'cosplay', 'workout', 'motivation', 'fyp', 'tourettes', 'foryou', 'tourettesawareness', 'lockdown', 'viral', 'funny', 'trending', 'painting', 'artistsoftiktok', 'trending', 'fyp', '', 'foryou', 'fyp', 'foryoupage', 'trending', 'viral', 'tennis', 'football', 'tictactoe', 'challenge', 'xyzbca', 'funny', 'sports', 'foryou', 'fyp', 'foryoupage', 'tiktok', 'comedy', 'trending', 'trend', 'funny', '', 'stitch', 'fyp', 'foryoupage', 'lenovojustbeyou', 'petlife', 'petsoftiktok', 'trending', 'fyp', 'funny', 'babyanddad', 'cutetoddler', 'viral', 'trending', 'bunnies', 'foryou', 'girldad', 'comedy', 'trend', '4u', 'trending',
+      'fyp', 'fypage', 'twilightsaga', 'usman', 'usman', 'usman', 'usman', 'usman', 'usman', 'usman', 'usman', 'bilal', 'taylorlautner', 'taydome', 'robertpattinson', 'viral', 'couplegoals', 'fyp', 'foryou', 'mellocair', 'wdym', 'whatdoyoumeme', 'trending', 'homecooked', 'cashappinbio', 'minecraft', 'foryou', 'foryoupage', 'minecraftbuild', 'mc', 'build', 'viral', 'trending', 'fernandoarmy', 'foryou', 'fy', 'fouryoupage', 'viral', 'trend', 'trending', 'paperplane', 'tutorial', '', '', 'dancer', 'choreography', 'trending', 'slumberparty', 'ashnikko', 'lgbt', 'brisbane', 'twerk', 'kmstudios', 'australia', 'dancetok', 'justiceforjohnnydepp', 'partnerwork', 'lesbian', '', 'trending', 'featureme', 'featurethis', 'viral', 'fyp', 'comedy', 'foryoupage', 'foryou', 'animal', 'lion', 'tiger', 'fyp', 'edit', 'pet', 'cat', 'fup', 'animals', 'nature', 'wild', 'wildlife', 'viral', 'zoo', 'trend', 'trending', 'safari', 'foru', 'awareness', 'learn', 'learnontiktok', 'edutok', 'education', '', 'ctcvoicebox', 'dayinmylife', 'oikosonetrip', 'rock', 'soul', 'fyp', 'cover', '365days', 'iseered', 'sing', 'singer', 'foryou', 'viral', 'trending', 'xyzbca', 'fyp', 'foryou', 'foryoupage', 'catsoftiktok', 'fantheory', 'tiktokfitness', 'trending', 'duet', '', 'satisfying', 'asmr', 'crushing', 'fyp', 'crushit', 'foryoupage', 'glass', 'trending', 'foryou', 'fyp', 'foryoupage', 'fyp', 'tiktok', 'viral', 'funny', 'trending', 'trend', 'fyp', 'fypシ', 'funny', 'funnyclip', 'meme', 'comedy', 'viral', 'trending', 'strange', 'lmao', 'hilarious', 'dailydoseofinternet', 'xyzabc']
+
 
 def create_connection(sqlite_file=None):
-
     try:
         conn = psycopg2.connect(
             user="tiktokapi",
@@ -17,15 +19,12 @@ def create_connection(sqlite_file=None):
             database="TikTokApi",
         )
         return conn
-
     except psycopg2.DatabaseError as error:
-        raise (
-            "An error is occured while connecting to the datebase",
-            error
-        )
+        raise error
+
 
 def create_table(header, db_name, file_name=None):
-    
+
     # for initial storage
     c_db_header = ""
     len_for_comma = 1
@@ -37,9 +36,9 @@ def create_table(header, db_name, file_name=None):
         if len(header) != len_for_comma:
             c_db_header += c_header + ", "
         else:
-            c_db_header += c_header  
+            c_db_header += c_header
         len_for_comma += 1
-        
+
     try:
         c.execute(f"""CREATE TABLE {db_name}({c_db_header})""")
         print(f'new table {db_name} created')
@@ -48,12 +47,11 @@ def create_table(header, db_name, file_name=None):
 
     conn.commit()
     conn.close()
-    
+
+
 def insert_data(header, tiktok_data, db_name):
-    
     conn = create_connection()
     c = conn.cursor()
-
     try:
         for db_data in tiktok_data:
             c.execute(f"""
@@ -65,24 +63,76 @@ def insert_data(header, tiktok_data, db_name):
                 c.execute(f"""
                     INSERT INTO {db_name}({str(header[0])})
                     VALUES ('{db_data}')
-                    ON CONFLICT ({str(header[0])}) DO UPDATE SET {str(header[1])} = (SELECT {str(header[1])} FROM {db_name} where {str(header[0])} = '{db_data}')  + 1;
+                    ON CONFLICT ({str(header[0])}) 
+                    DO UPDATE SET {str(header[1])} = (SELECT {str(header[1])} FROM {db_name} where {str(header[0])} = '{db_data}')  + 1;
                 """)
     except psycopg2.OperationalError as error:
         raise error
-
     conn.commit()
     conn.close()
+
+# insert_data()
+
+
+def get_data(header, db_name):
+    top_hashtags = []
+    conn = create_connection()
+    c = conn.cursor()
+    try:
+        c.execute(f"""
+            SELECT {str(header[0])}, {str(header[1])} FROM {db_name}
+            WHERE {str(header[3])} IS NULL
+            limit 5;
+            """)
+        top_hashtags_1 = c.fetchall()
+        if c.rowcount >= 1:
+            print(
+                "  <---- getting hashtags from null DateTime in database { 5 random hashtags }---->")
+            print("  <---- Updating updated DataTime for next prediction ---->")
+            for update_datetime in top_hashtags_1:
+                c.execute(f"""
+                    UPDATE {db_name} SET {str(header[3])} = '{datetime.now()}'
+                    WHERE {str(header[0])} = '{update_datetime[0]}' AND {str(header[1])} = {update_datetime[1]}
+                """)
+        if c.rowcount == 0:
+            print(
+                "  <---- If not null DateTime then take data updated DateTime { 5 random hashtags }---->")
+            c.execute(f"""
+                SELECT {str(header[0])}, {str(header[1])} FROM {db_name}
+                ORDER BY {str(header[3])}
+                limit 5;
+            """)
+            data_available = c.fetchall()
+            print("  <---- Updating updated DataTime for next prediction ---->")
+            for update_datetime in data_available:
+                c.execute(f"""
+                    UPDATE {db_name} SET {str(header[3])} = '{datetime.now()}'
+                    WHERE {str(header[0])} = '{update_datetime[0]}' AND {str(header[1])} = {update_datetime[1]}
+                """)
+        if top_hashtags_1:
+            top_hashtags = top_hashtags_1
+        elif data_available:
+            top_hashtags = data_available
+
+    except psycopg2.OperationalError as error:
+        raise error
+    conn.commit()
+    conn.close()
+    print('     successfully gotted 5 oldest hashtags \n')
+    return top_hashtags
+
 
 def single_search(header, search_data, db_name, file_name=""):
     conn = create_connection(file_name)
     c = conn.cursor()
     print(db_name)
-    d = c.execute("select * from '%s' where %s = %s" %(db_name, header, search_data))
-    # d = c.execute("select * from %s where %s" %(db_name, search_data))
+    d = c.execute("select * from '%s' where %s = %s" %
+                  (db_name, header, search_data))
     return d
 
+
 def remove_rows(header, db_name, file_name=None):
-     # for initial storage
+    # for initial storage
     c_db_header = ""
     len_for_comma = 1
     conn = create_connection(file_name)
@@ -93,7 +143,7 @@ def remove_rows(header, db_name, file_name=None):
         if len(header) != len_for_comma:
             c_db_header += f"'{c_header}'" + ", "
         else:
-            c_db_header += c_header  
+            c_db_header += c_header
         len_for_comma += 1
     sql = f"delete from {db_name} where {c_db_header} not null"
     print(sql)
@@ -104,17 +154,19 @@ def remove_rows(header, db_name, file_name=None):
     conn.commit()
     conn.close()
     print('closed')
-    
+
+
 def drop_table(db_name, file_name=None):
     conn = create_connection(file_name)
     c = conn.cursor()
     try:
-        c.execute("drop table %s" %(db_name))
+        c.execute("drop table %s" % (db_name))
     except sqlite3.OperationalError as e:
-        return {'message': "Table not fount!", 'error' : e.args[0]}
+        return {'message': "Table not fount!", 'error': e.args[0]}
     conn.commit()
     conn.close()
     return True
+
 
 table_row = [
     'Pid AUTOINCREMENT PRIMARY KEY',
@@ -125,9 +177,10 @@ table_row = [
     # 'Age int',
 ]
 
-header = ['hashtag', 'count_hashtag', 'created_at',]
-db_ = "video_editor_hashtag"
+header = ['hashtag', 'count_hashtag', 'created_at', 'updated_at', ]
+table_name = "video_editor_hashtag"
 # print(create_table(table_row, 'new_table2'))
-print(insert_data(header, dd, db_))
+# print(insert_data(header, dd, table_name))
+# print(get_data(header, table_name))
 # print(drop_table('new_table2'))
 # print(remove_rows(['name',], 'test_hashtag'))
--- tiktok/hashtag.py
+++ tiktok/hashtag.py
@@ -1,87 +1,38 @@
 from TikTokApi import TikTokApi
 import random
-import string
-from db import insert_data
+from db import get_data, insert_data
 
 
-def download_video(video_id, file_format=None):
-    with TikTokApi() as api:
-        video = api.video(id=str(video_id))
-        video_byte = video.bytes()
-        random_name = "".join((random.choice(string.ascii_letters) for i in range(10)))
-        with open(f"videos/{random_name}.mp4", 'wb') as file:
-            file.write(video_byte)
-            print('video downloadeded...')
-
-def hashtag():
-    api = TikTokApi()
-    tag = api.hashtag(name="pubg")
-    return tag
-
-
-def save_hashtag():
-    
+def save_api_data():
     # Database info
-    header = ['hashtag', 'count_hashtag', 'created_at',]
-    auto_generate_hashtag = []
-    table_name = "video_editor_hashtag"
+    hashtag_header = ['hashtag', 'count_hashtag', 'created_at', 'updated_at',]
+    videoid_header = ['video_id', 'created_at', 'updated_at',]
+    hashtag_names = []
+    video_id = []
+    hashtag_table = "video_editor_hashtag"
+    video_id_table = "video_editor_videoid"
     
     # Initlize tiktok API
     api = TikTokApi()
     
-    # create empty list
-    tag = api.hashtag(name="pubg")
-
-    for v in tag.videos(count=10):
+    # get oldest hashtags
+    top_hashtags = get_data(hashtag_header, hashtag_table)
+    if top_hashtags:
+        random_hashtag = random.choice(top_hashtags)
+        print('randome tag', random_hashtag[0])
+        tag = api.hashtag(name=random_hashtag[0])
+    else:
+        tag = api.hashtag(name="рекомендации")
+        
+    for v in tag.videos():
         json_v = v.as_dict
+        video_id.append(json_v['id'])
         for hashtag in json_v['textExtra']:
-            auto_generate_hashtag.append(hashtag['hashtagName'])
+            hashtag_names.append(hashtag['hashtagName'])
     
-    print(len(auto_generate_hashtag), auto_generate_hashtag)
+    print("total hashtags:", len(hashtag_names), len(video_id), video_id)
     # database arguments
-    insert_data(header, auto_generate_hashtag, table_name)
+    insert_data(hashtag_header, hashtag_names, hashtag_table)
 
-save_hashtag()
-
-
-def hashtag():
-    
-    downloaded_list = []
-    video_downloaded_status = False
-    auto_generate_hashtag = []
-    api = TikTokApi()
-
-    tag = api.hashtag(name="trending")
-    
-    count = 0
-
-    for v in tag.videos(count=10):
-        
-        print('counting ', count, 'v-id', v.id)
-        
-        json_v = v.as_dict
-        # for video_ID in downloaded_list:
-        for video_ID in json_v:
-            if video_ID != str(json_v['id']):
-                if json_v['id'] not in downloaded_list:
-                    print('Id not match, Video will download')
-                    downloaded_list.append(v.id)
-                    for hashtag in json_v['textExtra']:
-                        if hashtag not in auto_generate_hashtag:
-                            auto_generate_hashtag.append(hashtag['hashtagName'])
-                    download_video(v.id)
-                    video_downloaded_status += True
-                    count += 1
-        
-        if count == 2:
-            print(downloaded_list)
-            print(auto_generate_hashtag)
-            print(len(auto_generate_hashtag))
-            break
-    
-    header = ['video_id',]
-    res = add_data(header, auto_generate_hashtag, 'tiktok_hashtag')
-    print('save hashtag ', res)
-    print(count)
-
-# hashtag()
+if __name__ == "__main__":
+    save_api_data()
